11ad61c6351fbb82b9b169682d241b1498e31675
nik <nameissubjecttochange@gmail.com>
2025-03-27 21:34:57 -0400

    Update ylib.py

--- ylib.py
+++ ylib.py
@@ -2,27 +2,30 @@ import requests
 import json
 
 def getBookInfo(isbn):
-    url = f"https://openlibrary.org/isbn/{isbn}"
+    url = f"https://openlibrary.org/api/books?bibkeys={isbn}&format=json&jscmd=data"
     data = {}
 
     response = requests.get(url, headers = {"accept": "application/json"})
     book_data = response.text
     # turn json to dict
-    # data = json.loads(book_data)
+    data = json.loads(book_data)
+    data = data[isbn]
     with open(f"{isbn}.json", "w") as file:
         file.write(book_data)
     
+    parsed_data = {
+        "isbn": isbn,
+        "title": data["title"],
+        "author": data["authors"][0]["name"],
+        "pages": data["pagination"],
+        "publisher": data["publishers"][0]["name"],
+        "publish_date": data["publish_date"],
+        "cover_url": data["cover"]
+    }
+    print(parsed_data)
 
-def getCover(key, value, size):
-    # key = isbn, value = isbn number, size = s,m,or l
-    url = f"https://covers.openlibrary.org/b/{key}/{value}-{size}.jpg"
-    #this retrieves the actual image
-    response = requests.get(url)
-    with open(f"{value}.jpg", "wb") as file:
-        file.write(response.content)
-
-#isbn = "0441569595"
-isbn = "9781368098175"
+isbn = "0441569595"
+#isbn = "9781368098175"
 getBookInfo(isbn)
 getCover("isbn", isbn, "L")
 
